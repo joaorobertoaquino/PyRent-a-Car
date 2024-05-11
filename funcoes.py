78ba5ca447c3795ad53d3f23185d46276a426a71
joaorobertoaquino <galvaojoaoroberto@gmail.com>
2024-05-11 01:58:42 +0000

    módulos reserva, relatório e informações adicionados no arquivo funções

--- funcoes.py
+++ funcoes.py
@@ -17,10 +17,10 @@ def menuPrincipal():
     print("#####      0 - Sair                    #####")
     op_pric = input("##### Escolha sua opção: ")
     return op_pric
-
-########################################################  
-#####               Módulo Cliente                 #####  
-########################################################  
+    
+###############################################  
+#####          Módulo Cliente             #####  
+###############################################  
 def modCliente():
     os.system("clear")
     print()
@@ -37,18 +37,73 @@ def modCliente():
     input("Tecle <ENTER> para continuar...") 
     return op_cliente
 
+def cadastrarCliente():
+    os.system("clear")
+    print()
+    print("#############################################")
+    print("#####        Cadastrar Cliente         #####")
+    print("#############################################")
+    print()
+    nome = input("##### Nome: ")
+    print()
+    cpf = input("##### CPF: ")
+    print()
+    email = input("##### Email: ")
+    print()
+    fone = input("##### Celular: ")
+    print()
+    print("Aluno cadastrado com sucesso!")
+    input("Tecle <ENTER> para continuar...")
 
-######################################
-#####     Cadastrar Cliente      #####
-###################################### 
+def exibirDadosCliente():
+    os.system("clear")
+    print()
+    print("############################################")
+    print("#####      Exibe Dados do Cliente      #####")
+    print("############################################")
+    print()
+    print("##### Nome: ")
+    print("##### CPF: ")
+    print("##### Email: ")
+    print("##### Celular: ")
+    print()
+    input("Tecle <ENTER> para continuar...")
 
+def alterarDadosCliente():
+    os.system("clear")
+    print()
+    print("############################################")
+    print("#####      Alterar Dados do Cliente    #####")
+    print("############################################")
+    print()
+    nome = input("##### Nome: ")
+    print()
+    cpf = input("##### CPF: ")
+    print()
+    email = input("##### Email: ")
+    print()
+    fone = input("##### Celular: ")
+    print()
+    print("Dados alterados com sucesso!")
+    input("Tecle <ENTER> para continuar...")
 
-
-
-
-########################################################
-#####              Módulo Funcionários             #####
-########################################################    
+def excluirCliente():
+    os.system("clear")
+    print()
+    print("############################################")
+    print("#####         Excluir Cliente          #####")
+    print("############################################")
+    print()
+    nome = input("##### Nome: ")
+    print()
+    cpf = input("##### CPF: ")
+    print()
+    print("Aluno excluído com sucesso!")
+    input("Tecle <ENTER> para continuar...")
+    
+############################################### 
+#####          Módulo Funcionários        #####        
+###############################################    
 def modFunc():
     os.system("clear")
     print()
@@ -65,9 +120,73 @@ def modFunc():
     input("Tecle <ENTER> para continuar...")
     return op_func
 
-########################################################
-#####               Módulo Veículos                #####
-########################################################
+def cadastrarFunc():
+    os.system("clear")
+    print()
+    print("#############################################")
+    print("#####      Cadastrar Funcionário        #####")
+    print("#############################################")
+    print()
+    nome = input("##### Nome: ")
+    print()
+    cpf = input("##### CPF: ")
+    print()
+    email = input("##### Email: ")
+    print()
+    fone = input("##### Celular: ")
+    print()
+    print("Funcionário cadastrado com sucesso!")
+    input("Tecle <ENTER> para continuar...")
+
+def exibirDadosFunc():
+    os.system("clear")
+    print()
+    print("############################################")
+    print("#####   Exibir Dados do Funcionário    #####")
+    print("############################################")
+    print()
+    print("##### Nome: ")
+    print("##### Idade: ")
+    print("##### Email: ")
+    print("##### Celular: ")
+    print()
+    input("Tecle <ENTER> para continuar...")
+
+def alterarDadosFunc():
+    os.system("clear")
+    print()
+    print("############################################")
+    print("#####   Alterar Dados do Funcionário   #####")
+    print("############################################")
+    print()
+    nome = input("##### Nome: ")
+    print()
+    cpf = input("##### CPF: ")
+    print()
+    email = input("##### Email: ")
+    print()
+    fone = input("##### Celular: ")
+    print()
+    print("Dados alterados com sucesso!")
+    input("Tecle <ENTER> para continuar...")
+
+def excluirFunc():
+    os.system("clear")
+    print()
+    print("############################################")
+    print("#####       Excluir Funcionário        #####")
+    print("############################################")
+    print()
+    nome = input("##### Nome: ")
+    print()
+    cpf = input("##### CPF: ")
+    print()
+    print("Funcionário excluído com sucesso!")
+    input("Tecle <ENTER> para continuar...") 
+
+###########################################
+#####         Módulo Veículos         #####         
+###########################################
 def modVeic():
     os.system('clear')    
     print()
@@ -84,13 +203,134 @@ def modVeic():
     input("Tecle <ENTER> para continuar...")
     return op_veic
 
-#######################################################
-#####                Módulo Reserva               #####
-#######################################################
+def cadastrarVeic():
+    os.system('clear')
+    print()
+    print("############################################")
+    print("#####        Cadastrar Veículo         #####")
+    print("############################################")
+    modelo = input("##### Modelo: ")
+    print()
+    ano = input("##### Ano de lançamento: ")
+    print()
+    cor = input("##### Cor: ")
+    print()
+    chassi = input("##### Número do Chassi: ")
+    print()
+    placa = input("##### Número da placa: ")
+    print()
+    print("Veículo cadastrado com sucesso!")
+    input("Tecle <ENTER> para continuar...")
 
+def exibirDadosVeic():
+    os.system('clear')
+    print()
+    print("############################################")
+    print("#####    Exibe Dados do Veículo       #####")
+    print("############################################")
+    print()
+    print("##### Modelo: ")
+    print("##### Ano de lançamento: ")
+    print("##### Cor: ")
+    print("##### Número do Chassi: ")
+    print("##### Número da placa: ")
+    print()
+    input("Tecle <ENTER> para continuar...")
 
+def alterarDadosVeic():
+    os.system('clear')
+    print()
+    print("############################################")
+    print("#####    Alterar Dados do Veículo      #####")
+    print("############################################")
+    modelo = input("##### Modelo: ")
+    print()
+    ano = input("##### Ano de lançamento: ")
+    print()
+    cor = input("##### Cor: ")
+    print()
+    chassi = input("##### Número do Chassi: ")
+    print()
+    placa = input("##### Número da placa: ")
+    print()
+    print("Dados alterados com sucesso!")
+    input("Tecle <ENTER> para continuar...")
 
+def excluirVeic():
+    os.system('clear')
+    print()
+    print("############################################")
+    print("#####       Exclui Veículo             #####")
+    print("############################################")
+    print()
+    modelo = input("##### Modelo: ")
+    print()
+    ano = input("##### Ano de lançamento: ")
+    print()
+    cor = input("##### Cor: ")
+    print()
+    chassi = input("##### Número do Chassi: ")
+    print()
+    placa = input("##### Número da placa: ")
+    print()
+    print("Veículo excluído com sucesso!")
+    input("Tecle <ENTER> para continuar...")
 
-######################################################
-#####             Módulo Informações             #####
-######################################################
+############################################
+#####          Módulo Reserva          #####        
+############################################
+def modReserva():
+    os.system('clear')
+    print()
+    print("#############################################")
+    print("#####   Você está no Módulo Reserva     #####")
+    print("#############################################")
+    print("##### 1 - Lista de Veículos Disponíveis #####")
+    print("##### 2 - Lista de Preços               #####")
+    print("##### 3 - Opções de Seguro              #####")
+    print("##### 4 - Formas de Pagamento           #####")
+    print("##### 5 - Política de Combustível       #####")
+    print("##### 6 - Suporte ao Cliente            #####")
+    print("##### 0 - Retornar ao Menu Principal    #####")
+    op_reserva = input("##### Escolha sua opção: ")
+    print()
+    input("Tecle <ENTER> para continuar...")
+    return op_reserva
+
+#############################################
+#####         Módulo Relatório          #####   
+#############################################
+def modRelatorio():
+    os.system('clear')
+    print()
+    print("#############################################")
+    print("#####   Você está no Módulo Relatório   #####")
+    print("#############################################")
+    print("##### 1 - Lista Geral de Clientes       #####")
+    print("##### 2 - Lista Geral de Funcionários   #####")
+    print("##### 3 - Lista Geral de Veículos       #####")
+    print("##### 4 - Lista de Veículos Alugados    #####")
+    print("##### 5 - Veículos mais Procurados      #####")
+    print("##### 0 - Retornar ao Menu Principal    #####")
+    op_relatorio = input("##### Escolha sua opção: ")
+    print()
+    input("Tecle <ENTER> para continuar            #####")
+    return op_relatorio 
+
+   
+##############################################
+#####         Módulo Informações         #####    
+##############################################
+def modInfo():
+    os.system('clear')
+    print()
+    print("#############################################")
+    print("#####  Você está no Módulo Informações  #####")
+    print("#############################################")
+    print("##### Projeto   -  Locadora de Veículos #####")
+    print("##### Desenvolvimento:                  #####")
+    print("##### João Roberto Galvão Aquino        #####")
+    print("##### E-mail para comunicação:          #####")
+    print("##### joao.roberto.galvao.017@gmail.com #####")
+    print()
+    input("Tecle <ENTER> para voltar ao menu principal...")
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import os
 from funcoes import (
+    alterarDadosFunc,
+    cadastrarFunc,
+    cadastrarVeic,
+    excluirFunc,
+    excluirVeic,
+    exibirDadosFunc,
+    exibirDadosVeic,
     menuPrincipal,
     modCliente,
+    cadastrarCliente,
+    excluirCliente,
+    exibirDadosCliente,
+    alterarDadosCliente,
     modFunc,
-    modVeic
+    modInfo,
+    modRelatorio,
+    modReserva,
+    modVeic,
+    alterarDadosVeic
 )
 
 ###################################################
@@ -18,262 +33,66 @@ while op_pric != '0':
     op_cliente = ''
     while op_cliente != '0':
       op_cliente = modCliente()
-          
+      
       if op_cliente == '1':
-        os.system("clear")
-        print()
-        print("#############################################")
-        print("#####        Cadastrar Cliente         #####")
-        print("#############################################")
-        print()
-        nome = input("##### Nome: ")
-        print()
-        cpf = input("##### CPF: ")
-        print()
-        email = input("##### Email: ")
-        print()
-        fone = input("##### Celular: ")
-        print()
-        print("Aluno cadastrado com sucesso!")
-        input("Tecle <ENTER> para continuar...")
+        op_cliente = cadastrarCliente()
 
       elif op_cliente == '2':
-        os.system("clear")
-        print()
-        print("############################################")
-        print("#####      Exibe Dados do Cliente      #####")
-        print("############################################")
-        print()
-        print("##### Nome: ")
-        print("##### CPF: ")
-        print("##### Email: ")
-        print("##### Celular: ")
-        print()
-        input("Tecle <ENTER> para continuar...")
+        op_cliente = exibirDadosCliente()
 
       elif op_cliente == '3':
-        os.system("clear")
-        print()
-        print("############################################")
-        print("#####      Altera Dados do Cliente     #####")
-        print("############################################")
-        print()
-        nome = input("##### Nome: ")
-        print()
-        cpf = input("##### CPF: ")
-        print()
-        email = input("##### Email: ")
-        print()
-        fone = input("##### Celular: ")
-        print()
-        print("Dados alterados com sucesso!")
-        input("Tecle <ENTER> para continuar...")
+        op_cliente = alterarDadosCliente()
   
       elif op_cliente == '4':
-        os.system("clear")
-        print()
-        print("############################################")
-        print("#####         Exclui Cliente           #####")
-        print("############################################")
-        print()
-        nome = input("##### Nome: ")
-        print()
-        cpf = input("##### CPF: ")
-        print()
-        print("Aluno excluído com sucesso!")
-        input("Tecle <ENTER> para continuar...")
-
-    
+        op_cliente = excluirCliente()
+   
   elif op_pric == '2':
     op_func = ''
     while op_func != '0':
       op_func = modFunc()
            
       if op_func == '1':
-        os.system("clear")
-        print()
-        print("#############################################")
-        print("#####      Cadastrar Funcionários       #####")
-        print("#############################################")
-        print()
-        nome = input("##### Nome: ")
-        print()
-        cpf = input("##### CPF: ")
-        print()
-        email = input("##### Email: ")
-        print()
-        fone = input("##### Celular: ")
-        print()
-        print("Funcionário cadastrado com sucesso!")
-        input("Tecle <ENTER> para continuar...")
+        op_func = cadastrarFunc()
 
       elif op_func == '2':
-        os.system("clear")
-        print()
-        print("############################################")
-        print("#####    Exibe Dados do Funcionário    #####")
-        print("############################################")
-        print()
-        print("##### Nome: ")
-        print("##### Idade: ")
-        print("##### Email: ")
-        print("##### Celular: ")
-        print()
-        input("Tecle <ENTER> para continuar...")
+        op_func = exibirDadosFunc()
 
       elif op_func == '3':
-        os.system("clear")
-        print()
-        print("############################################")
-        print("#####   Alterar Dados do Funcionário   #####")
-        print("############################################")
-        print()
-        nome = input("##### Nome: ")
-        print()
-        cpf = input("##### CPF: ")
-        print()
-        email = input("##### Email: ")
-        print()
-        fone = input("##### Celular: ")
-        print()
-        print("Dados alterados com sucesso!")
-        input("Tecle <ENTER> para continuar...")
+        op_func = alterarDadosFunc()
 
       elif op_func == '4':
-        os.system("clear")
-        print()
-        print("############################################")
-        print("#####       Exclui Funcionário         #####")
-        print("############################################")
-        print()
-        nome = input("##### Nome: ")
-        print()
-        cpf = input("##### CPF: ")
-        print()
-        print("Funcionário excluído com sucesso!")
-        input("Tecle <ENTER> para continuar...")
+        op_func = excluirFunc()
 
-  
   elif op_pric == '3':
     op_veic = ''
-    while op_veic != 0:
+    while op_veic != '0':
       op_veic = modVeic()
 
       if op_veic == '1':
-        os.system('clear')
-        print()
-        print("############################################")
-        print("#####        Cadastrar Veículo        #####")
-        print("############################################")
-        modelo = input("##### Modelo: ")
-        print()
-        ano = input("##### Ano de lançamento: ")
-        print()
-        cor = input("##### Cor: ")
-        print()
-        chassi = input("##### Número do Chassi: ")
-        print()
-        placa = input("##### Número da placa: ")
-        print()
-        print("Veículo cadastrado com sucesso!")
-        input("Tecle <ENTER> para continuar...")
+        op_veic = cadastrarVeic()
 
       elif op_veic == '2':
-        os.system('clear')
-        print()
-        print("############################################")
-        print("#####    Exibe Dados do Veículo       #####")
-        print("############################################")
-        print()
-        print("##### Modelo: ")
-        print("##### Ano de lançamento: ")
-        print("##### Cor: ")
-        print("##### Número do Chassi: ")
-        print("##### Número da placa: ")
-        print()
-        input("Tecle <ENTER> para continuar...")
+        op_veic = exibirDadosVeic()
 
       elif op_veic == '3':
-        os.system('clear')
-        print()
-        print("############################################")
-        print("#####    Alterar Dados do Veículo      #####")
-        print("############################################")
-        modelo = input("##### Modelo: ")
-        print()
-        ano = input("##### Ano de lançamento: ")
-        print()
-        cor = input("##### Cor: ")
-        print()
-        chassi = input("##### Número do Chassi: ")
-        print()
-        placa = input("##### Número da placa: ")
-        print()
-        print("Dados alterados com sucesso!")
-        input("Tecle <ENTER> para continuar...")
+        op_veic = alterarDadosVeic()
 
       elif op_veic == '4':
-        os.system('clear')
-        print()
-        print("############################################")
-        print("#####       Exclui Veículo             #####")
-        print("############################################")
-        print()
-        modelo = input("##### Modelo: ")
-        print()
-        ano = input("##### Ano de lançamento: ")
-        print()
-        cor = input("##### Cor: ")
-        print()
-        chassi = input("##### Número do Chassi: ")
-        print()
-        placa = input("##### Número da placa: ")
-        print()
+        op_veic = excluirVeic()
     
   elif op_pric == '4':
-   print()
-   print("#############################################")
-   print("#####   Você está no Módulo Reserva     #####")
-   print("#############################################")
-   print("##### 1 - Lista de Veículos Disponíveis #####")
-   print("##### 2 - Lista de Preços               #####")
-   print("##### 3 - Opções de Seguro              #####")
-   print("##### 4 - Formas de Pagamento           #####")
-   print("##### 5 - Política de Combustível       #####")
-   print("##### 6 - Suporte ao Cliente            #####")
-   print("##### 0 - Retornar ao Menu Principal    #####")
-   op_pric = input("##### Escolha sua opção: ")
-   print()
-   input("Tecle <ENTER> para continuar...")
+   op_reserva = ''
+   while op_reserva != '0':
+     op_reserva = modReserva()
 
   elif op_pric == '5':
-    print()
-    print("#############################################")
-    print("#####   Você está no Módulo Relatório   #####")
-    print("#############################################")
-    print("##### 1 - Lista Geral de Clientes       #####")
-    print("##### 2 - Lista Geral de Funcionários   #####")
-    print("##### 3 - Lista Geral de Veículos       #####")
-    print("##### 4 - Lista de Veículos Alugados    #####")
-    print("##### 5 - Veículos mais Procurados      #####")
-    print("##### 0 - Retornar ao Menu Principal    #####")
-    op_pric = input("##### Escolha sua opção: ")
-    print()
-    input("Tecle <ENTER> para continuar           #####")
+    op_relatorio = ''
+    while op_relatorio != '0':
+      op_relatorio = modRelatorio()
+     
 
   elif op_pric == '6':
-    print()
-    print("#############################################")
-    print("#####  Você está no Módulo Informações  #####")
-    print("#############################################")
-    print("##### Projeto   -  Locadora de Veículos #####")
-    print("##### Desenvolvimento:                  #####")
-    print("##### João Roberto Galvão Aquino        #####")
-    print("##### E-mail para comunicação:          #####")
-    print("##### joao.roberto.galvao.017@gmail.com #####")
-    print()
-    input("Tecle <ENTER> para continuar...")
-
+    op_pric = modInfo()
 
 print("Você encerrou o programa!")
 print("Até logo!")
